e4c1d118f327a1f9cf5ca147a103a90a94f30454
shonali-ks <50069308+shonali-ks@users.noreply.github.com>
2020-06-19 12:27:38 +0530

    Update server.js

--- Collaborative-editor/server.js
+++ Collaborative-editor/server.js
@@ -7,7 +7,7 @@ var multer = require('multer');
 var bodyParser = require('body-parser');
 
 const  port = process.env.PORT || 5000;
-
+const users = {}
 /* App */
 server.listen(port, () => {
     console.log('Server listening at port %d', port);
@@ -18,9 +18,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Handle socket traffic
 io.on('connection', (socket) => {
     // Relay chat data to all clients
+   
+    socket.on('new-user', name => {
+        users[socket.id] = name
+        socket.broadcast.emit('user-connected', name)
+      })
+      
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+      })
+      
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected', users[socket.id])
+        delete users[socket.id]
+      })
+    
     socket.on('myInputUpdate', function(data) {
         socket.broadcast.emit('myInputUpdate', data);
     });
 });
-
 
