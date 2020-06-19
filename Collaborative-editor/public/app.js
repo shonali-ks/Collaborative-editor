a237ffee3096cf1394651a276cea13eff3b80c06
shonali-ks <50069308+shonali-ks@users.noreply.github.com>
2020-06-19 22:02:52 +0530

    Update app.js

--- Collaborative-editor/public/app.js
+++ Collaborative-editor/public/app.js
@@ -1,4 +1,4 @@
- const socket = io('http://localhost:5000')
+const socket = io('http://localhost:5000')
 var testarea = document.getElementById("testing-area");
 
 const messageContainer = document.getElementById('message-container')
